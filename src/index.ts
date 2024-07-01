29fe51824b0762719ad0791719cbfa14fa05001f
Shell1125 <haroldflanagan999@gmail.com>
2024-07-01 04:35:08 -0400

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+//For legacy compatibility purposes, expose the node bits as default API
+import {getRedHatService, RedHatService, TelemetryService, TelemetryEvent, IdProvider} from './node';
+export {getRedHatService, RedHatService, TelemetryService, TelemetryEvent, IdProvider};
